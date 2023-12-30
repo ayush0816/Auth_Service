6fc05f31ce45aaef0636dff926ec0e3a6bd70478
Ayush Singh <ayush.singh@beepkart.com>
2023-12-30 22:29:40 +0530

    Added initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const { PORT } = require("./config/serverConfig");
+
+const app = express();
+
+const prepareAndStartServer = ()=> {
+
+    app.listen(3001,()=>{
+        console.log("Started the server on port :", 3001);
+    })
+};
+
+prepareAndStartServer();
